742c6615a52228663359d638021d030f8ad25616
Youxiang <98683010+Dong237@users.noreply.github.com>
2023-04-19 11:04:29 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,12 +1,1 @@
-# Configuration file for the Sphinx documentation builder.
-
-# -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
-
 
